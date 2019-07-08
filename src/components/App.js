7052fe17f9e423da872f2ea3f2bd03505105842f
David Jesus De Freitas Rosales <ddefreitas@ve00000pf0sw4eb.ml.com>
2019-07-08 15:42:00 -0400

    agregada llamada a función y css para el proyecto, actualmente es stateless o componente funcional

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,55 @@
 import React from 'react';
 
 const App = () => {
+  
+  const ponerFilas = () => [
+      <tr>
+        <td>
+          Rodolfo
+        </td>
+        <td>
+          rodolfo@saldivar.com
+        </td>
+        <td>
+          Rodolfo.com
+        </td>
+      </tr>,
+      <tr>
+        <td>
+          Platzi
+        </td>
+        <td>
+          platzi@platzi.com
+        </td>
+        <td>
+          platzi.com
+        </td>
+      </tr>
+  ]
+  
   return (
-    <div>
-      Hola
+    
+    <div className='margen'>
+      <table className='tabla'>
+        <thead>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <th>
+              Correo
+            </th>
+            <th>
+              Enlace
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          { ponerFilas() }
+        </tbody>
+      </table>
     </div>
+      
   );
 }
 
