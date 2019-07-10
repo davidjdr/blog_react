fb72cda9e6a4c1f9bd55cce253c38c3ad642d7b0
David Jesus De Freitas Rosales <ddefreitas@ve00000pf0sw4eb.ml.com>
2019-07-10 13:55:09 -0400

    se agrega store y reducers

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
+
+const store = createStore(
+    reducers, //todos los reducers, 
+    {} // estado inicial
+)
+
+
 ReactDOM.render(
-<App />, document.getElementById('root')
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById('root')
 );
 
