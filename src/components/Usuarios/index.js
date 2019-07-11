421dbcd63dd7c126dc6161251e38c44a9fceb05c
David Jesus De Freitas Rosales <ddefreitas@ve00000pf0sw4eb.ml.com>
2019-07-11 13:42:16 -0400

    creacion de componente funcional para agregar spinner

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import Spinner from '../General/Spinner'
 
 import * as usuariosActions from '../../actions/usuariosActions';
 
@@ -11,6 +12,33 @@ class Usuarios extends Component {
 
   componentDidMount() {
     this.props.traerTodos()
+  }
+
+  ponerContenido = () => {
+    if (this.props.cargando) {
+      return <Spinner />
+    }
+    
+    return (
+        <table className='tabla'>
+          <thead>
+            <tr>
+              <th>
+                Nombre
+              </th>
+              <th>
+                Correo
+              </th>
+              <th>
+                Enlace
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.ponerFilas() }
+          </tbody>
+        </table>
+    );
   }
 
   ponerFilas = () => (
@@ -32,24 +60,7 @@ class Usuarios extends Component {
   render () {
     return (
       <div>
-        <table className='tabla'>
-          <thead>
-            <tr>
-              <th>
-                Nombre
-              </th>
-              <th>
-                Correo
-              </th>
-              <th>
-                Enlace
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.ponerFilas() }
-          </tbody>
-        </table>
+        { this.ponerContenido() }
       </div>
         
     );
