46f95b53bc4f4e0f62a1803521c9a06857808815
David Jesus De Freitas Rosales <ddefreitas@ve00000pf0sw4eb.ml.com>
2019-07-12 11:14:27 -0400

    agregado componente para manejo de publicaciones, paso de parametros por url

--- src/components/Usuarios/Tabla.js
+++ src/components/Usuarios/Tabla.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Tabla = (props) => {
-    const ponerFilas = () => props.usuarios.map((usuario) => (
+    const ponerFilas = () => props.usuarios.map((usuario, key) => (
           <tr key={usuario.id} >
             <td>
               {usuario.name}
@@ -12,6 +13,11 @@ const Tabla = (props) => {
             </td>
             <td>
               {usuario.website}
+            </td>
+            <td>
+                <Link to={`/publicaciones/${key}`}>
+                    <div className="eye-solid icon"></div>
+                </Link>
             </td>
           </tr>
       ));
